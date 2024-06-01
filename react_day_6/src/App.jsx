cd3ba85dc53c02a9ee9e6f046eeac2178350f015
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-06-01 12:00:04 -0500

    completing the day!

--- react_day_6/src/App.jsx
+++ react_day_6/src/App.jsx
@@ -1,25 +1,30 @@
 
 import './App.css';
-import SignIn from './pages/SignIn';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import React from 'react'
-
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Wiki from './pages/Wiki';
+import Pokemon from './pages/Pokemon';
+// import ErrorPage from './pages/ErrorPage';
 
 
 const router = createBrowserRouter([
+  
   {
     path: "/home",
     element: <Layout />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
     children: [
       { path: "", element: <Home /> },
       { path: "pokemon/:pokemonID", element: <Pokemon /> },
       { path: "wiki", element: <Wiki /> },
     ]
-  },
+  }, //separate objects by commas
   {
     path: "/",
     element: <SignIn />
